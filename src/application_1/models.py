cc2014bcc3ce2aa6e39fb772e7148f7a61ed6d21
nova-sangeeth <novasangeeth@outlook.com>
2020-03-25 17:03:27 +0530

    Created new models and added templates to the application.

--- src/application_1/models.py
+++ src/application_1/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Registeration(models.Model):
+    gender_choices = (
+        ('MALE', 'The Gender is Male'),
+        ('FEMALE', 'The Gender is female'),
+        ('OTHERS', 'The Gender is others'),
+        ('NONE', 'The Gender NONE')
+    )
+    First_name = models.CharField(max_length=256, null=True, blank=False)
+    Last_name = models.CharField(max_length=256, null=True, blank=False)
+    Gender = models.CharField(
+        max_length=128, choices=gender_choices, default='NONE', blank=False)
+    Email = models.EmailField(unique=True)
+    Phone = models.CharField(
+        unique=True, null=True, blank=False, max_length=13)
+    City = models.CharField(max_length=128, blank=False)
+    Username = models.CharField(
+        max_length=128, blank=False, unique=True, null=True)
+    Password = models.CharField(max_length=128, blank=False, null=True)
+
+    def __str__(self):
+        return self.First_name
--- src/interview_project/settings.py
+++ src/interview_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'application_1',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
