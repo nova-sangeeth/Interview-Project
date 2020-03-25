a1ff715e028b0ea49e9b4ff2d3e6c21c3b1b6bad
nova-sangeeth <novasangeeth@outlook.com>
2020-03-26 01:34:50 +0530

    admin title changes

--- src/application_1/admin.py
+++ src/application_1/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+
+
+admin.site.site_header = 'Interview Project Site Administration Login '
+admin.site.site_title = 'Interview Project Site Administration Panel'
+admin.site.index_title = 'Interview Project Site Dashboard.'
